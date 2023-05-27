20f0a2f5df34dc9133d759650f75b9b6cca2935e
jmartymar <jeffjmart@gmail.com>
2023-05-27 10:50:37 -0500

    moderation service working

--- comments/index.js
+++ comments/index.js
@@ -17,25 +17,41 @@ app.get('/posts/:id/comments', (req, res) => {
 app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
-
   const comments = commentsByPostId[req.params.id] || [];
-
-  comments.push({ id: commentId, content });
-
+  comments.push({ id: commentId, content, status: 'pending' });
   await axios.post('http://localhost:4005/events', {
     type: 'CommentCreated',
     data: {
       id: commentId,
       content,
-      postId: req.params.id
+      postId: req.params.id,
+      status: 'pending'
     }
   });
   commentsByPostId[req.params.id] = comments;
   res.status(201).send(commentsByPostId[req.params.id]);
 });
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Received Event: ', req.body.type);
+  const { type, data } = req.body;
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        postId,
+        id,
+        status,
+        content
+      }
+    });
+  }
   res.send({});
 });
 
