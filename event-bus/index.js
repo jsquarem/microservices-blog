e0dca221d220cc98cc17707cd2ae5637c18184fb
jmartymar <jeffjmart@gmail.com>
2023-05-28 14:37:02 -0500

    kubernetes deployment working

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,28 +5,30 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
   const event = req.body;
-
+  events.push(event);
   axios.post('http://localhost:4000/events', event).catch((err) => {
     console.log(err.message);
   });
-
   axios.post('http://localhost:4001/events', event).catch((err) => {
     console.log(err.message);
   });
-
   axios.post('http://localhost:4002/events', event).catch((err) => {
     console.log(err.message);
   });
-
   axios.post('http://localhost:4003/events', event).catch((err) => {
     console.log(err.message);
   });
-
   res.send({ status: 'OK' });
 });
 
-app.listen(4005, () => {
+app.get('/events', (req, res) => {
+  res.send(events);
+});
+
+app.listen(4005, async () => {
   console.log('Listening on 4005');
 });
