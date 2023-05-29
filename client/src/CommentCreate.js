3adb000e9ce16f6c5f26328184e4f1aa884fdd65
jmartymar <jeffjmart@gmail.com>
2023-05-29 12:00:17 -0500

    app working on kube

--- client/src/CommentCreate.js
+++ client/src/CommentCreate.js
@@ -5,7 +5,7 @@ const CommentCreate = ({ postId }) => {
   const [content, setContent] = useState('');
   const onSubmit = async (event) => {
     event.preventDefault();
-    await axois.post(`http://localhost:4001/posts/${postId}/comments`, {
+    await axois.post(`http://posts.com/posts/${postId}/comments`, {
       content
     });
     setContent('');
